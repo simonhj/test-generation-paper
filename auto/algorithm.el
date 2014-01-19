24a95aab276244b26822a8d01b1f66f30471e62c
Simon Jensen <simon@hjensen.net>
2014-01-19 14:56:28 -0800

    Writing in implementation

---
+++ auto/algorithm.el
@@ -0,0 +1,10 @@
+(TeX-add-style-hook "algorithm"
+ (lambda ()
+    (LaTeX-add-labels
+     "sec:control-flow-graph"
+     "fig:cfga"
+     "fig:cfgb"
+     "fig:cfgc"
+     "fig:cfg"
+     "sec:backw-subst")))
+
